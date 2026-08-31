99fce8d4bb010a9e43cc8f4318c63e1b43bcca02
Gustavo Kreischer de Almeida <gustavokreischer@gmail.com>
2026-08-31 10:36:14 -0300

    fix: ci github actions

--- sos_app/karma.conf.js
+++ sos_app/karma.conf.js
@@ -4,50 +4,62 @@
 module.exports = function (config) {
   config.set({
     basePath: "",
+
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
+
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage"),
     ],
+
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: false,
     },
+
     jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
+      suppressAll: true,
     },
+
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/app"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [
+        { type: "html" },
+        { type: "text-summary" },
+      ],
     },
+
     reporters: ["progress", "kjhtml"],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: process.env.CI ? ["ChromeHeadlessNoSandbox"] : ["Chrome"],
+
+    autoWatch: !process.env.CI,
+
+    browsers: process.env.CI
+      ? ["ChromeHeadlessNoSandbox"]
+      : ["Chrome"],
+
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: "ChromeHeadless",
         flags: [
           "--no-sandbox",
+          "--disable-setuid-sandbox",
           "--disable-gpu",
-          "--disable-translate",
-          "--disable-extensions",
           "--disable-dev-shm-usage",
-          "--remote-debugging-port=9222"
+          "--no-first-run",
+          "--no-zygote",
+          "--disable-extensions",
         ],
       },
     },
-    singleRun: true,
+
+    singleRun: !!process.env.CI,
     restartOnFileChange: false,
   });
 };
